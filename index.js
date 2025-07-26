e4dbc7038229bef9819243173e1da83e72edb430
xinhanlim <xhxhan00@gmail.com>
2025-07-26 04:12:43 +0000

    added db-test;

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const db = require('./database')
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Routes
-app.get('/', (req, res) => {
-  res.json({ message: "Welcome to the API" });
+app.get('/db-test', (req, res) => {
+  try{
+    res.send({
+      "message":"Connected Successfully"
+    })
+  } catch(e){
+    console.log(e);
+    res.status(500).json({
+      "message":"Mysql connection failed"
+    })
+  }
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3400;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
